3c69472899ecf27da01faf9a43f8779ed3c0f611
David Omurwa <91884298+dewardvide@users.noreply.github.com>
2024-03-05 23:19:27 +0100

    Feature update
    1. Added MISP class
    2. Added config file to house API key and MISP IP
    3. Added test argument and misp_i object in main class for testing purposes

--- IoC_EoL_Checker.py
+++ IoC_EoL_Checker.py
@@ -1,9 +1,33 @@
 import requests
 import argparse
-import jarm
+#import jarm
 import os 
 import subprocess
+import json
 
+class MISP:
+    # Class attributes for MISP IP and Auth Key
+    MISP_Ip = None
+    MISP_Auth_Key = None
+
+    @classmethod
+    def set_config(cls, MISP_Ip, MISP_Auth_Key):
+        # Set the class attributes with the provided values
+        cls.MISP_Ip = MISP_Ip
+        cls.MISP_Auth_Key = MISP_Auth_Key
+
+    def __init__(self):
+        # Load configuration from JSON file if class attributes are not set
+        if not MISP.MISP_Ip or not MISP.MISP_Auth_Key:
+            with open('config.json') as f:
+                Config_Items = json.load(f)
+        
+            MISP.set_config(Config_Items.get('MISP_IP'), Config_Items.get('MISP_AUTH_KEY'))
+    
+    def print_test(self):
+        print(MISP.MISP_Ip)
+        print(MISP.MISP_Auth_Key)
+ 
 def Object_Enrichment(): 
     return 
 
@@ -26,7 +50,8 @@ def main():
 
     # Add arguments
     parser.add_argument('-u', '--update', action='store_true', help='Update JARM.')
-    parser.add_argument('-f', '--flag', action='store_true', help='Description of the flag.')
+    parser.add_argument('-t', '--test', action='store_true', help='Used for testing.')
+    
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -40,8 +65,9 @@ def main():
     if args.update: 
         update()
 
-    if args.flag:
-        print("Flag is set.")
+    if args.test:
+        misp_i = MISP()
+        misp_i.print_test()
         
 if __name__ == "__main__":
     main()
