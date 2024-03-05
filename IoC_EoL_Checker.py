a91dba3fc8cb6ee37c1a61a4dc3f7706fd379633
David Omurwa <91884298+dewardvide@users.noreply.github.com>
2024-03-05 18:55:19 +0100

    Feature Update
    The following was added to the main script:
    1. Argument Parsing
    2. The update function (it doesn't work)

---
+++ IoC_EoL_Checker.py
@@ -0,0 +1,41 @@
+import requests
+import argparse
+import os 
+
+def update():
+    download_status = ''
+    update_response = requests.get(':https://github.com/03bee4a8-135d-4d93-8125-cd84ecf5b50')
+    if update_response.status_code == 200:
+        with open('jarm.py', 'wb') as f:
+            f.write(update_response.content)
+            download_status =  "Update Complete"
+    else: 
+        download_status = "Update Failed. Status Code: "+update_response.status_code
+
+    return download_status
+
+def main(): 
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description='IOC EOL CHECKER => Analyze Changes in the JARM signature in MISP Objects')
+
+    # Add arguments
+    parser.add_argument('-u', '--update', action='store_true', help='Update JARM.')
+    parser.add_argument('-f', '--flag', action='store_true', help='Description of the flag.')
+    #parser.add_argument('-o', '--optional', type=int, help='Description of an optional argument.')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Access the arguments
+    if args.update: 
+        update()
+
+    if args.flag:
+        print("Flag is set.")
+
+    #if args.optional:
+    #    print("Optional argument is set to:", args.optional)
+        
+main()
+
+
